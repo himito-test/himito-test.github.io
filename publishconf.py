28239b0e7587d9eff04cd6be7ad58fd378a7f688
Jaime Arias <jaime.arias@inria.fr>
2016-11-24 16:09:43 +0100

    fixing url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://himito.github.io'
+SITEURL = 'https://himito-test.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
